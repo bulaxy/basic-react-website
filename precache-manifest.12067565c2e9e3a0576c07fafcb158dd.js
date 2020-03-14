d0f42df81911a7b0b9a547456bfd3396e4bd7d37
XPS13Window <rogerhiufungcheng@gmail.com>
2020-03-14 12:37:18 +1100

    Updates

---
+++ precache-manifest.12067565c2e9e3a0576c07fafcb158dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f68d86da51d1739a52265b02cb1c2a",
+    "url": "/basic-react-website/index.html"
+  },
+  {
+    "revision": "954192b7639994150e39",
+    "url": "/basic-react-website/static/css/main.d9da9e0f.chunk.css"
+  },
+  {
+    "revision": "f02aec6368de4313243a",
+    "url": "/basic-react-website/static/js/2.a1317692.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/basic-react-website/static/js/2.a1317692.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954192b7639994150e39",
+    "url": "/basic-react-website/static/js/main.a50c79ca.chunk.js"
+  },
+  {
+    "revision": "d3cda56a910303d2eaf8",
+    "url": "/basic-react-website/static/js/runtime-main.7fa80e5e.js"
+  }
+]);
