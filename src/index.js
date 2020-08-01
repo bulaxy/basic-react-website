882b4cdde73d598ce222c1bd1054e98a79e4087e
Bulaxy <rogerhiufungcheng@gmail.com>
2020-08-01 17:04:59 +1000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import HomePage from "./views/HomePage/HomePage";
+// import AboutMe from "views/AboutMe/AboutMe.js";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={hist}>
+			<Switch>
+				<Route path="/" component={HomePage} />
+				{/* <Route path="/aboutme" component={AboutMe} /> */}
+			</Switch>
+		</Router>
+	</Provider>,
+	document.getElementById("root")
+);
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import xxx from './xxxReducer.js';
+
+const RootReducers = combineReducers({
+	xxx,
+});
+
+export default RootReducers;
