07b9e682fdc38d0eb06c28e0f40f413468e434ad
Bulaxy <rogerhiufungcheng@gmail.com>
2023-08-22 20:38:49 +1000

    chore: Temp try

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Router, Route, Switch } from "react-router-dom";
-import { Provider } from 'react-redux';
-import store from './store';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import HomePage from "./views/HomePage/HomePage";
-// import AboutMe from "views/AboutMe/AboutMe.js";
-
-var hist = createBrowserHistory();
-
-ReactDOM.render(
-	<Provider store={store}>
-		<Router history={hist}>
-			<Switch>
-				<Route path="/" component={HomePage} />
-				{/* <Route path="/aboutme" component={AboutMe} /> */}
-			</Switch>
-		</Router>
-	</Provider>,
-	document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import xxx from './xxxReducer.js';
-
-const RootReducers = combineReducers({
-	xxx,
-});
-
-export default RootReducers;
